794c3ea493f5606428ca6f7c2bafc96c662af990
mohammedsalah7 <mhamad_m@windowslive.com>
2021-06-30 16:29:37 +0300

    add java script to quiz app
     add questions files

---
+++ questions.js
@@ -0,0 +1,115 @@
+let questions = [
+    {
+    numb: 1,
+    question: "What does HTML stand for?",
+    answer: "Hyper Text Markup Language",
+    options: [
+      "Hyper Text Preprocessor",
+      "Hyper Text Markup Language",
+      "Hyper Text Multiple Language",
+      "Hyper Tool Multi Language"
+    ]
+  },
+    {
+    numb: 2,
+    question: "What does CSS stand for?",
+    answer: "Cascading Style Sheet",
+    options: [
+      "Common Style Sheet",
+      "Colorful Style Sheet",
+      "Computer Style Sheet",
+      "Cascading Style Sheet"
+    ]
+  },
+    {
+    numb: 3,
+    question: "What Is The Capital Of Palestine ?",
+    answer: "Jerusalem",
+    options: [
+      "London",
+      "Jerusalem",
+      "Cairo",
+      "Doha"
+    ]
+  },
+    {
+    numb: 4,
+    question: "What does SQL stand for?",
+    answer: "Structured Query Language",
+    options: [
+      "Stylish Question Language",
+      "Stylesheet Query Language",
+      "Statement Question Language",
+      "Structured Query Language"
+    ]
+  },
+    {
+    numb: 5,
+    question: "What does XML stand for?",
+    answer: "eXtensible Markup Language",
+    options: [
+      "eXtensible Markup Language",
+      "eXecutable Multiple Language",
+      "eXTra Multi-Program Language",
+      "eXamine Multiple Language"
+    ]
+  },
+  {
+    numb: 6,
+    question: "What is  the corect about ===?",
+    answer: "Compare type and value",
+
+    options: [
+      "Not Fast",
+      "Compare type and value",
+      "Compare type just",
+      "Compare value just"
+
+    ]
+  },
+  {
+    numb: 7,
+    question: "What is the result of multiplying 32 * 65?",
+    answer: "2080",
+    options: [
+      "8020",
+      "3265",
+      "2080",
+      "all of above"
+    ]
+  },
+  {
+    numb: 8,
+    question: "In which club does Mohamed Salah play?",
+    answer: "Liverpool",
+    options: [
+      "A.C Milane",
+      "Barcelonae",
+      "Bayern Munich",
+      "Liverpool"
+    ]
+  },
+  {
+    numb: 9,
+    question: "True && (false || true && (true|| false))",
+    answer: "True",
+    options: [
+      "False",
+      "True",
+      "Error",
+      "Not of above"
+    ]
+  },
+  {
+    numb: 10,
+    question: "whene i need to return Array which a i should ues?",
+    answer: "map",
+    options: [
+      "find",
+      "fillter",
+      "map",
+      "All of above"
+    ]
+  },
+  
+];
